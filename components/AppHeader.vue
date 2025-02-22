b31464b1de438155be157be433f5b29d64c5ff7f
Beanmommo <leonardo.prasetyo5@gmail.com>
2025-02-22 17:18:35 +1100

    Added mobile responsive page for web responsiveness

---
+++ components/AppHeader.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="app-header">
+        <div class="app-item-left">
+            Work
+        </div>
+        <div class="app-item-center">
+            <img src="/public/logo.png" alt="logo" width="50px" />
+        </div>
+        <div class="app-item-right">
+            <div>
+                About
+            </div>
+            <div>
+                Contact
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.app {
+    &-header {
+        position: sticky;
+        top: 0;
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        column-gap: $margin;
+        align-items: center;
+        justify-content: space-around;
+
+        .app-item {
+            font-size: 24px;
+            font-weight: bold;
+
+            &-left {
+                text-align: left;
+            }
+
+            &-center {
+                text-align: center;
+            }
+
+            &-right {
+                display: flex;
+                justify-content: flex-end;
+                gap: 0.5rem;
+                text-align: right;
+
+            }
+
+        }
+    }
+
+
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ["@/assets/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/global.scss";',
+        },
+      },
+    },
+  },
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "Noto Sans": true,
+      "Noto Serif": true,
+    },
+    download: true,
+  },
+});
